1e41a7d6590ea8b4757b27e6bba57ab110db5fbe
pductrungg <trung.pham2023@hcmut.edu.vn>
2024-06-26 10:30:25 +0700

    final customer form

--- src/components/CustomerForm/CF.js
+++ src/components/CustomerForm/CF.js
@@ -1,4 +1,4 @@
-import {useState, useCallback} from 'react';
+import {useState, useCallback, useEffect} from 'react';
 import {Form, Input, Modal, DatePicker, Select, InputNumber, Col, Row} from 'antd';
 import dayjs from 'dayjs';
 import {LEAVE_TIME_OPTIONS, LEAVE_TIME_HOUR, GET_DAY_FUNC_TYPE, WORKING_TIME} from '../../constant';
@@ -21,14 +21,17 @@ const CF = ({
     const [isHiddenField, setIsHiddenField] = useState(true);
     const [formValues, setFormValues] = useState(null);
     const [isOpenLeaveHourField, setIsOpenLeaveHourField] = useState(false);
+    const [endDate,setEndDay]=useState(null);
+    const [startDate,setStartDay]=useState(null);
+    const [timeDiff,setTimeDiff]=useState('');
     
-    const numRange = useCallback((start, end) => {
-        const res = [];
-        for(let i=start; i<=end; i++){
-            res.push(i);
-        }
-        return res;
-    }, []);
+    // const numRange = useCallback((start, end) => {
+    //     const res = [];
+    //     for(let i=start; i<=end; i++){
+    //         res.push(i);
+    //     }
+    //     return res;
+    // }, []);
 
     const reset = useCallback(() => {
         setLeaveTime(null);
@@ -41,93 +44,93 @@ const CF = ({
         setFormValues(form.getFieldsValue(true));
     }, [form]);
 
-    const calDayNoWK = useCallback((start_day, numDay = 1) => {
-        // let end_day = dayjs(start_day)
-        let end_day = dayjs(start_day).add(numDay,'day')
-         for(let i=numDay+1; checkWeekend(end_day);i++){
-            end_day = dayjs(start_day).add(i,'day');
-         }
-         return end_day;
-    }, []);
+    // const calDayNoWK = useCallback((start_day, numDay = 1) => {
+    //     // let end_day = dayjs(start_day)
+    //     let end_day = dayjs(start_day).add(numDay,'day')
+    //      for(let i=numDay+1; checkWeekend(end_day);i++){
+    //         end_day = dayjs(start_day).add(i,'day');
+    //      }
+    //      return end_day;
+    // }, []);
 
-    const calculateDay = useCallback((start, end) => {
-        let totalHours = 0;
-        let timePerDay = [];
-        if(checkWeekend(start) && checkWeekend(end)){
-            return totalHours;
-        }
+    // const calculateDay = useCallback((start, end) => {
+    //     let totalHours = 0;
+    //     let timePerDay = [];
+    //     if(checkWeekend(start) && checkWeekend(end)){
+    //         return totalHours;
+    //     }
 
-        let fromDatetoString = getDayToCalcTime(GET_DAY_FUNC_TYPE.TO_JSON, start);
-        let toDateString = getDayToCalcTime(GET_DAY_FUNC_TYPE.TO_JSON, end);
+    //     let fromDatetoString = getDayToCalcTime(GET_DAY_FUNC_TYPE.TO_JSON, start);
+    //     let toDateString = getDayToCalcTime(GET_DAY_FUNC_TYPE.TO_JSON, end);
 
-        if(fromDatetoString === toDateString){
-            totalHours = calMeetHours(start, end);
-            timePerDay.push({
-                leaveDate: dayjs(start)
-                .set('h', WORKING_TIME.NEW_DAY[0])
-                .set('m',WORKING_TIME.NEW_DAY[1])
-                .set('s',WORKING_TIME.NEW_DAY[2]),
-                totalH: totalHours,
-            });
-        }else{
-            let day_cal = dayjs(start);
+    //     if(fromDatetoString === toDateString){
+    //         totalHours = calMeetHours(start, end);
+    //         timePerDay.push({
+    //             leaveDate: dayjs(start)
+    //             .set('h', WORKING_TIME.NEW_DAY[0])
+    //             .set('m',WORKING_TIME.NEW_DAY[1])
+    //             .set('s',WORKING_TIME.NEW_DAY[2]),
+    //             totalH: totalHours,
+    //         });
+    //     }else{
+    //         let day_cal = dayjs(start);
 
-            for (let i=1; fromDatetoString !== toDateString; i++){
-                if(checkWeekend(day_cal)){
-                    day_cal = dayjs(start).add(i, 'day');
-                    fromDatetoString = getDayToCalcTime(GET_DAY_FUNC_TYPE.TO_JSON, day_cal);
-                    continue;
-                }
+    //         for (let i=1; fromDatetoString !== toDateString; i++){
+    //             if(checkWeekend(day_cal)){
+    //                 day_cal = dayjs(start).add(i, 'day');
+    //                 fromDatetoString = getDayToCalcTime(GET_DAY_FUNC_TYPE.TO_JSON, day_cal);
+    //                 continue;
+    //             }
 
-                if(i === 1){
-                    let leaveTime = calMeetHours(
-                        day_cal,
-                        getDayToCalcTime(GET_DAY_FUNC_TYPE.END_DAY,day_cal)
-                    );
-                    totalHours += leaveTime;
-                    timePerDay.push({
-                        leaveDate: dayjs(day_cal)
-                            .set('h',WORKING_TIME.NEW_DAY[0])
-                            .set('m',WORKING_TIME.NEW_DAY[1])
-                            .set('s',WORKING_TIME.NEW_DAY[2]),
-                        totalH: leaveTime,
-                    });
-                } else {
-                    let leaveTime = calMeetHours(
-                        getDayToCalcTime(GET_DAY_FUNC_TYPE.START_DAY, day_cal),
-                        getDayToCalcTime(GET_DAY_FUNC_TYPE.END_DAY, day_cal)
-                    );
-                    totalHours += leaveTime;
-                    timePerDay.push({
-                        leaveDate: dayjs(day_cal)
-                            .set('h',WORKING_TIME.NEW_DAY[0])
-                            .set('m',WORKING_TIME.NEW_DAY[1])
-                            .set('s',WORKING_TIME.NEW_DAY[2]),
-                        totalH: leaveTime,
-                    });
-                }
-                day_cal = dayjs(start).add(i, 'day');
-                fromDatetoString = getDayToCalcTime(GET_DAY_FUNC_TYPE.TO_JSON, day_cal);
-            }
+    //             if(i === 1){
+    //                 let leaveTime = calMeetHours(
+    //                     day_cal,
+    //                     getDayToCalcTime(GET_DAY_FUNC_TYPE.END_DAY,day_cal)
+    //                 );
+    //                 totalHours += leaveTime;
+    //                 timePerDay.push({
+    //                     leaveDate: dayjs(day_cal)
+    //                         .set('h',WORKING_TIME.NEW_DAY[0])
+    //                         .set('m',WORKING_TIME.NEW_DAY[1])
+    //                         .set('s',WORKING_TIME.NEW_DAY[2]),
+    //                     totalH: leaveTime,
+    //                 });
+    //             } else {
+    //                 let leaveTime = calMeetHours(
+    //                     getDayToCalcTime(GET_DAY_FUNC_TYPE.START_DAY, day_cal),
+    //                     getDayToCalcTime(GET_DAY_FUNC_TYPE.END_DAY, day_cal)
+    //                 );
+    //                 totalHours += leaveTime;
+    //                 timePerDay.push({
+    //                     leaveDate: dayjs(day_cal)
+    //                         .set('h',WORKING_TIME.NEW_DAY[0])
+    //                         .set('m',WORKING_TIME.NEW_DAY[1])
+    //                         .set('s',WORKING_TIME.NEW_DAY[2]),
+    //                     totalH: leaveTime,
+    //                 });
+    //             }
+    //             day_cal = dayjs(start).add(i, 'day');
+    //             fromDatetoString = getDayToCalcTime(GET_DAY_FUNC_TYPE.TO_JSON, day_cal);
+    //         }
 
-            if(fromDatetoString === toDateString){
-                if(!checkWeekend(day_cal)){
-                    let leaveTime = calMeetHours(
-                        getDayToCalcTime(GET_DAY_FUNC_TYPE.START_DAY, day_cal),
-                        end
-                    );
-                    totalHours += leaveTime;
-                    timePerDay.push({
-                        leaveDate: dayjs(end)
-                            .set('h',WORKING_TIME.NEW_DAY[0])
-                            .set('m',WORKING_TIME.NEW_DAY[1])
-                            .set('s', WORKING_TIME.NEW_DAY[2]),
-                        totalH: leaveTime,
-                    });
-                }
-            }
-        }return {totalHours,timePerDay};
-    }, []);
+    //         if(fromDatetoString === toDateString){
+    //             if(!checkWeekend(day_cal)){
+    //                 let leaveTime = calMeetHours(
+    //                     getDayToCalcTime(GET_DAY_FUNC_TYPE.START_DAY, day_cal),
+    //                     end
+    //                 );
+    //                 totalHours += leaveTime;
+    //                 timePerDay.push({
+    //                     leaveDate: dayjs(end)
+    //                         .set('h',WORKING_TIME.NEW_DAY[0])
+    //                         .set('m',WORKING_TIME.NEW_DAY[1])
+    //                         .set('s', WORKING_TIME.NEW_DAY[2]),
+    //                     totalH: leaveTime,
+    //                 });
+    //             }
+    //         }
+    //     }return {totalHours,timePerDay};
+    // }, []);
 
     // const handleChange = (value) => {
     //     if(value > 0){
@@ -231,58 +234,91 @@ const CF = ({
     //     ]);
     // };
 
-    const ChangeTime = () =>{
-        if(form.getFieldValue('leaveTime') === LEAVE_TIME_HOUR.TWO_DAYS){
-            if(!!form.getFieldValue('goFrom')){
-                let dayApplyToDate = calDayNoWK(form.getFieldValue('goFrom'));
-
-                form.setFieldValue(
-                    'dayBack',
-                    dayjs(dayApplyToDate)
-                    .set('h',WORKING_TIME.END[0])
-                    .set('m',WORKING_TIME.END[1])
-                    .set('s',WORKING_TIME.END[2])
-                );
-
-                let hourPerDay = [
-                    {
-                        leaveDate: dayjs(form.getFieldValue('goFrom'))
-                            .set('h',WORKING_TIME.NEW_DAY[0])
-                            .set('m',WORKING_TIME.NEW_DAY[1])
-                            .set('s',WORKING_TIME.NEW_DAY[2]),
-                        totalH: LEAVE_TIME_HOUR.A_DAY,
-                    },
-                    {
-                        leaveDate: dayjs(dayApplyToDate)
-                            .set('h',WORKING_TIME.NEW_DAY[0])
-                            .set('m',WORKING_TIME.NEW_DAY[1])
-                            .set('s',WORKING_TIME.NEW_DAY[2]),
-                        totalH: LEAVE_TIME_HOUR.A_DAY,  
-                    },
-                ];
-
-                form.setFieldValue('hourPerDay', hourPerDay)
-            }else{
-                form.resetFields(['dayBack','hourPerDay']);
-            }
+    const start_time = (date) => {
+        setStartDay(date);
+        if(date){
+            // setEndDay(endDate ? date.set('h',endDate.hour()).set('m',endDate.minute()):date);
+            const EndSameDate = endDate ? date.set('h',endDate.hour()).set('m',endDate.minute()):date;
+            setEndDay(EndSameDate);
+            form.setFieldsValue({
+                goFrom:date,
+                dayBack:EndSameDate,
+            });
+        
         }else {
-            if(!!form.getFieldValue('goFrom') && !!form.getFieldValue('dayBack')){
-                let leaveFromDate = form.getFieldValue('goFrom');
-                let leaveToDate = form.getFieldValue('dayBack');
-
-                const{totalHours, timePerDay} = calculateDay(leaveFromDate, leaveToDate);
-
-                if(totalHours > 0){
-                    form.setFieldValue('totalHour', totalHours);
-                    form.setFieldValue('hourPerDay',timePerDay);
-                }
-            }else {
-                if(!!form.getFieldValue('totalHour')){
-                    form.resetFields(['totalHour','hourPerDay']);
-                }
-            }
+            setEndDay(null);
+            form.resetFields(['dayBack']);
         }
     };
+
+    const end_time = (date) => {
+        setEndDay(date);
+        form.setFieldsValue({dayBack:date});
+    };
+
+    useEffect(() => {
+        if(startDate && endDate){
+            const diff = endDate.diff(startDate,'minute');
+            const hours = Math.round(diff/60);
+            // const minutes = diff % 60;
+            setTimeDiff(`${hours}`);
+        }else{
+            setTimeDiff('');
+        }
+    },[startDate,endDate]);
+
+    // const ChangeTime = () =>{
+    //     if(form.getFieldValue('leaveTime') === LEAVE_TIME_HOUR.TWO_DAYS){
+    //         if(!!form.getFieldValue('goFrom')){
+    //             let dayApplyToDate = calDayNoWK(form.getFieldValue('goFrom'));
+
+    //             form.setFieldValue(
+    //                 'dayBack',
+    //                 dayjs(dayApplyToDate)
+    //                 .set('h',WORKING_TIME.END[0])
+    //                 .set('m',WORKING_TIME.END[1])
+    //                 .set('s',WORKING_TIME.END[2])
+    //             );
+
+    //             let hourPerDay = [
+    //                 {
+    //                     leaveDate: dayjs(form.getFieldValue('goFrom'))
+    //                         .set('h',WORKING_TIME.NEW_DAY[0])
+    //                         .set('m',WORKING_TIME.NEW_DAY[1])
+    //                         .set('s',WORKING_TIME.NEW_DAY[2]),
+    //                     totalH: LEAVE_TIME_HOUR.A_DAY,
+    //                 },
+    //                 {
+    //                     leaveDate: dayjs(dayApplyToDate)
+    //                         .set('h',WORKING_TIME.NEW_DAY[0])
+    //                         .set('m',WORKING_TIME.NEW_DAY[1])
+    //                         .set('s',WORKING_TIME.NEW_DAY[2]),
+    //                     totalH: LEAVE_TIME_HOUR.A_DAY,  
+    //                 },
+    //             ];
+
+    //             form.setFieldValue('hourPerDay', hourPerDay)
+    //         }else{
+    //             form.resetFields(['dayBack','hourPerDay']);
+    //         }
+    //     }else {
+    //         if(!!form.getFieldValue('goFrom') && !!form.getFieldValue('dayBack')){
+    //             let leaveFromDate = form.getFieldValue('goFrom');
+    //             let leaveToDate = form.getFieldValue('dayBack');
+
+    //             const{totalHours, timePerDay} = calculateDay(leaveFromDate, leaveToDate);
+
+    //             if(totalHours > 0){
+    //                 form.setFieldValue('totalHour', totalHours);
+    //                 form.setFieldValue('hourPerDay',timePerDay);
+    //             }
+    //         }else {
+    //             if(!!form.getFieldValue('totalHour')){
+    //                 form.resetFields(['totalHour','hourPerDay']);
+    //             }
+    //         }
+    //     }
+    // };
 
     // const handleChangeTimePerDay = (value) => {
     //     if(value > 0){
@@ -486,15 +522,8 @@ const CF = ({
                             style={{
                                 width:'100%',
                             }}
-                            // disabledTime={
-                            //     !isOpenLeaveHourField
-                            //     ? () => ({
-                            //         disabledHours: () => numRange(0, 23),
-                            //         disabledMinutes: () => numRange(0, 59),
-                            //       })
-                            //     : null
-                            // }
-                            onChange={ChangeTime}
+                            onChange={start_time}
+                            value={startDate}
                             minuteStep={5}
                             popupClassName="CFDatePick"
                         />
@@ -532,25 +561,7 @@ const CF = ({
                             style={{
                                 width:'100%',
                             }}
-                            // disabledDate={(current) => {
-                            //     return(
-                            //         current &&
-                            //         form.getFieldValue('goFrom') &&
-                            //         current > form.getFieldValue('goFrom')
-                            //     );
-                            // }}
-                            ///////////////////////////////////////////////////////
-                            // disabledTime={
-                            //     !isOpenLeaveHourField
-                            //     ? () => ({
-                            //         disabledHours: () => numRange(0, 23),
-                            //         disabledMinutes: () => numRange(0, 59),
-                            //         })
-                            //     : null
-                            // }
-                            ////////////////////////////////////////////////////////
-                            // disabled={!isOpenLeaveHourField}
-                            ///////////////////////////////////////////////////////
+
                             disabledDate={(current) => {
                             const goFromDate = form.getFieldValue('goFrom');
                             return (
@@ -559,17 +570,8 @@ const CF = ({
                                 !current.isSame(goFromDate, 'day') // Only allow the same day
                             );
                             }}
-                            // disabledTime={(current) => {
-                            //     const goFromDate = form.getFieldValue('goFrom');
-                            //     if (!isOpenLeaveHourField || !goFromDate || !current) return {};
-                            //     const goFromHour = goFromDate.hour();
-                            //     const goFromMinute = goFromDate.minute();
-                            //     return {
-                            //       disabledHours: () => numRange(0, goFromHour),
-                            //       disabledMinutes: (selectedHour) => selectedHour === goFromHour ? numRange(0, goFromMinute) : [],
-                            //     };
-                            //   }}
-                            onChange={ChangeTime}
+                            onChange={end_time}
+                            value={endDate}
                             minuteStep={5}
                             popupClassName="CFDatePick"
                         />
@@ -614,24 +616,17 @@ const CF = ({
                     </Form.Item> */}
 
                     <Form.Item
-                        name="totalHour"
+                        // name="totalHour"
                         label="Số giờ gặp khách hàng"
                         rules={[
                             {
                                 required: true,
-                                message:'Nhập thời gian gặp khách hàng(số giờ)',
+                                message:'Nhập thời gian gặp khách hàng',
                             },
                         ]}
                         style={{marginBottom: '8px'}}
                     >
-                        <InputNumber
-                            style={{
-                                width:'100%'
-                            }}
-                            placeholder="Tổng số giờ gặp khách hàng"
-                            disabled
-                        >
-                        </InputNumber>
+                        <Input value={timeDiff} readOnly disabled/>
                     </Form.Item>
 
                     {/* {isOpenLeaveHourField && (
